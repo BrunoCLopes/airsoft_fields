f525639692a609ab5e038b5ffdba237dfc65b43f
BrunoCLopes <bruno.giuliani04@live.com>
2025-10-16 11:11:28 -0300

    File form validation

--- static/js/form_validation.js
+++ static/js/form_validation.js
@@ -1,25 +1,47 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.querySelector('form');
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        const requiredFields = form.querySelectorAll('.required');
-        requiredFields.forEach(field => {
-            const formContainer = field.closest('.form-item');
-            if (!field.value.trim()) {
-                console.log(formContainer);
-                if (formContainer.querySelector('span')) return;
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("form");
 
-                const label = formContainer.querySelector('label').textContent;
-                const span = document.createElement('span');
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const requiredFields = form.querySelectorAll(".required");
 
-                span.textContent = `O campo ${label} é obrigatório.`;
-                span.classList.add('text-red-500', 'font-medium','text-sm', 'block', 'md:absolute');
-                formContainer.appendChild(span);
-                field.classList.add('outline-2', 'outline-red-500');
-            }else{
-                field.classList.remove('outline-2', 'outline-red-500');
-                formContainer.querySelector('span')?.remove();
-            }
-        });
-    });  
+    requiredFields.forEach((field) => {
+      const formContainer = field.closest(".form-item");
+
+      const label = formContainer.querySelector("label").textContent;
+      const span = document.createElement("span");
+
+      span.textContent = `O campo ${label} é obrigatório.`;
+      span.classList.add(
+        "empty-error",
+        "text-red-500",
+        "font-medium",
+        "text-sm",
+        "block",
+        "md:absolute"
+      );
+
+      if (field.id === "field-photo") {
+        if (field.files.length === 0) {
+          if (formContainer.querySelector(".empty-error")) return;
+
+          formContainer.appendChild(span);
+        }else{
+            formContainer.querySelector(".empty-error")?.remove();
+        }
+      } else {
+        if (!field.value.trim()) {
+          if (formContainer.querySelector(".empty-error")) return;
+
+          formContainer.appendChild(span);
+          field.classList.remove("outline", "outline-gray-300");
+          field.classList.add("outline-2", "outline-red-500");
+        } else {
+          field.classList.remove("outline-2", "outline-red-500");
+          field.classList.add("outline", "outline-gray-300");
+          formContainer.querySelector(".empty-error")?.remove();
+        }
+      }
+    });
+  });
 });
